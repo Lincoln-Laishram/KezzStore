9aeba091ed98433ae27e5c0e5c18f3a4c9a4bd8c
Lincoln-Laishram <laishramlincoln82@gmail.com>
2025-01-24 18:06:36 +0530

    Phase 2

--- src/Pages/HomePae.jsx
+++ src/Pages/HomePae.jsx
@@ -3,15 +3,18 @@ import img1 from "../assets/Pictures/emperor.png"
 export const HomePage = () => {
     return (
         <>
-            <NavBar />
-            <div className="border-solid border-black border-[1px] h-[300px] w-full">
-                <div className="">
-                    <img src={img1} className="h-[100px]" alt="photo" />
+            <div className="h-[350px] w-full relative z-10">
+                <NavBar />
+                <div className="h-[250px] w-full absolute top-[100px] z-[1] bg-white">
+                    <img src={img1} className="h-[100px] " alt="photo" />
                 </div>
             </div>
-            <div>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed tempora asperiores illo minima doloribus nemo error nisi laborum ullam quis consequuntur illum explicabo placeat aliquam dolorem aperiam, esse autem a!
+
+            {/* This div should remain in the normal document flow */}
+            <div className="relative">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ratione et necessitatibus neque ducimus assumenda ipsam at animi perferendis quia deserunt itaque obcaecati, debitis unde ipsum illo, nostrum ea tenetur.
             </div>
+
         </>
     )
 }
