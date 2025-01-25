84cdef07b92aa5d9f237d8fcdc0656ae6397feac
Lincoln-Laishram <laishramlincoln82@gmail.com>
2025-01-25 10:38:29 +0530

    Phase 5

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -17,16 +17,16 @@ export const Carousal = () => {
             spaceBetween={50}
             slidesPerView={1}
             effect="fade" // Enable the fade effect
-            autoplay={{
-                delay: 3000,
-                disableOnInteraction: false,
-            }}
+            // autoplay={{
+            //     delay: 3000,
+            //     disableOnInteraction: false,
+            // }}
             loop={true}
         >
 
-            <SwiperSlide className="border border-black bg-white p-4">
-                <div className="flex justify-evenly">
-                    <img className="h-[150px] w-[auto] relative left-[-24px]" src={img1} alt="Slide 2" />
+            <SwiperSlide className="border border-black bg-[white] p-6">
+                <div className="flex justify-evenly h-[200px]">
+                    <img className="h-[150px] w-[auto] relative left-[-24px] my-[auto]" src={img1} alt="Slide 2" />
                     <div className="my-[auto] w-[150px] ">
                         <p className="font-bold text-2xl">NeuroActiv6</p>
                         <p className="text-l text-[red]">80%</p>
@@ -37,9 +37,9 @@ export const Carousal = () => {
                 </div>
             </SwiperSlide>
 
-            <SwiperSlide className="border border-black bg-white p-4">
-                <div className="flex justify-evenly">
-                    <img className="h-[150px] w-[auto] relative left-[-24px]" src={img2} alt="Slide 2" />
+            <SwiperSlide className="border border-black bg-white p-6">
+                <div className="flex justify-evenly h-[200px]">
+                    <img className="h-[150px] w-[auto] relative left-[-24px] my-[auto]" src={img2} alt="Slide 2" />
                     <div className="my-[auto] w-[150px] ">
                         <p className="font-bold text-2xl">Kerassentials</p>
                         <p className="text-l text-[red]">40%</p>
@@ -50,9 +50,9 @@ export const Carousal = () => {
                 </div>
             </SwiperSlide>
 
-            <SwiperSlide className="border border-black bg-white p-4">
-                <div className="flex justify-evenly">
-                    <img className="h-[150px] w-[auto] relative left-[-24px]" src={img3} alt="Slide 2" />
+            <SwiperSlide className="border border-black bg-white p-6">
+                <div className="flex justify-evenly h-[200px]">
+                    <img className="h-[150px] w-[auto] relative left-[-24px] my-[auto]" src={img3} alt="Slide 2" />
                     <div className="my-[auto] w-[150px] ">
                         <p className="font-bold text-2xl">Pinealxt</p>
                         <p className="text-l text-[red]">80%</p>
