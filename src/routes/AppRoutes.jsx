36a0f07e5bd019c356f37a3d1bd6a2bf5eeaf48f
Lincoln-Laishram <laishramlincoln82@gmail.com>
2025-02-17 00:05:00 +0530

    Phone responsive done

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,8 +1,5 @@
 import { createBrowserRouter, Link } from "react-router-dom";
 import App from "../App";
-import { SignUp } from "../Components/SignUp";
-import { SignIn } from "../Components/SignIn";
-import { Profile } from "../Components/Profile";
 import { About } from "../Pages/About";
 import { Terms } from "../Pages/Term";
 const router = createBrowserRouter([
